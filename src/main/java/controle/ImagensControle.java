096b29bb2c85f42a617f951ead2167d9497b92b5
Daniel Rodrigues Alves <da691985@gmail.com>
2019-05-21 23:05:40 -0300

    Criptografia

--- src/main/java/controle/ImagensControle.java
+++ src/main/java/controle/ImagensControle.java
@@ -1,28 +1,38 @@
 package controle;
 
-import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.sql.DataSource;
+import java.util.ArrayList;
+import org.primefaces.model.UploadedFile;
 import modelo.ImagensModelo;
 
 public class ImagensControle {
-	public boolean insertImagem(DataSource ds, ImagensModelo im) throws SQLException {
-		boolean resultado = false;
-		Connection con = ds.getConnection();
-		String sql = "INSERT INTO imagens(imagem, nome, descrisao) VALUES(?, ?, ?);";
+	public ArrayList<ImagensModelo> consultaCartoes() throws SQLException, ClassNotFoundException{
+		ArrayList<ImagensModelo> lista = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CMS?user=root&password=123");
+		String sql = "SELECT * FROM teste;";
 		PreparedStatement ps = con.prepareStatement(sql);
-		ps.setBinaryStream(1, (InputStream) im.getFile());
-		ps.setString(2, im.getNome());
-		ps.setString(3, im.getDescrisao());
-		if (ps.execute()) {
-			resultado = true;
-		} else {
-			throw new SQLException("Problma em inserir informações.");
+		ResultSet rs = ps.executeQuery();
+		if(rs != null) {
+			lista = new ArrayList<ImagensModelo>();
+			while(rs.next()){
+				ImagensModelo mode = new ImagensModelo();
+				mode.setId(rs.getInt("id"));
+				mode.setFile((UploadedFile) rs.getBinaryStream("imagem"));
+				mode.setNome(rs.getString("nome"));
+				mode.setDescrisao(rs.getString("descrisao"));
+				lista.add(mode);
+			}
+		}else {
+			throw new SQLException("Erro ao buscar informações.");
 		}
 		con.close();
-		return resultado;
+		return lista;
 	}
+
 
 }
--- src/main/java/modelo/ImagensModelo.java
+++ src/main/java/modelo/ImagensModelo.java
@@ -1,12 +1,22 @@
 package modelo;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import org.primefaces.model.UploadedFile;
+
+
 
 @ManagedBean
 public class ImagensModelo {
@@ -14,6 +24,44 @@ public class ImagensModelo {
 	private UploadedFile file;
 	private String nome;
 	private String descrisao;
+	
+	
+	public String cifrar(String texto) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+		String resultado = null;
+			 Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		     byte[] mensagem = texto.getBytes();
+
+		     // Usando chave de 128-bits (16 bytes)
+		     byte[] chave = "chave de 16bytes".getBytes();
+
+		     // Encriptando...
+		     try {
+				cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(chave, "AES"));
+			} catch (InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		     byte[] encrypted = cipher.doFinal(mensagem);
+		     String resto = Base64.getEncoder().encodeToString(encrypted);
+		     resultado = resto;
+		return resultado;
+	}
+	public String decifrar(String texto) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		String resultado = null;
+		 Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+	    String textoB = new String(Base64.getDecoder().decode(texto));
+	    byte[] mensagem = textoB.getBytes();
+
+	    // Usando chave de 128-bits (16 bytes)
+	    byte[] chave = "chave de 16bytes".getBytes();
+	    
+	    // Decriptando...
+	    cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(chave, "AES"));
+	    byte[] decrypted = cipher.doFinal(mensagem);
+	    resultado = new String(decrypted);
+	    return resultado;
+	}
+
 	
 	public int getId() {
 		return id;
@@ -34,8 +82,10 @@ public class ImagensModelo {
 					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/CMS?user=root&password=123");
 					PreparedStatement ps = con.prepareStatement("INSERT INTO teste(imagem, nome, descrisao) VALUES(?, ?, ?);");
 					ps.setBinaryStream(1, file.getInputstream());
-					ps.setBytes(2, nome.getBytes());
-					ps.setBytes(3, descrisao.getBytes());
+					String nomeC = cifrar(nome);
+					String descrisaoC = cifrar(descrisao);
+					ps.setBytes(2, nomeC.getBytes());
+					ps.setBytes(3, descrisaoC.getBytes());
 					ps.execute();
 					con.close();
 					FacesMessage message = new FacesMessage("Exito", file.getFileName() + " Deu certo");
